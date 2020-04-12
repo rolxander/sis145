6e7f16b70792bfc613c5d2d9b966977da91fda0b
rolxander <gaelelpapi@gmail.com>
2020-04-11 23:35:18 -0400

    configwebpack

---
+++ src/App.js
@@ -0,0 +1,4 @@
+import './public/css/estilos.scss';
+
+const App=document.getElementById("app");
+App.innerHTML = "Hola mundo"
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+      context: __dirname,
+      entry: {
+          main: path.resolve(__dirname,"src/App.js")
+      },
+      output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'src/js[name][hash].js'
+      },module: {
+        rules: [
+          { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+          {
+            test: /\.css$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+                options: {              
+                  publicPath: '../'
+                }
+              },
+              "css-loader"]
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+                options: {                  
+                  publicPath: '../'
+                }
+              },
+              "css-loader","sass-loader"
+            ]
+          },         
+        ]
+      },
+      plugins : [
+        new HtmlWebpackPlugin({
+          template:"./index.html",
+          filename:'./index.html'
+        }),
+        new MiniCssExtractPlugin({
+          filename: "./src/css/[name].[hash].css",
+          chunkFilename: "css/[id].[hash].css"
+        })
+      ]
+      
+}
